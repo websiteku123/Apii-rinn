97597adeebd24c3a35979d83facbbcf502335694
websiteku123 <kedebongtunas@gmail.com>
2026-07-07 11:52:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
 // ==========================================
 // 2. MIDDLEWARE INJECT CREATOR RESPONSE
 // ==========================================
-const CREATOR = process.env.API_CREATOR || "Api By Rinn";
+const CREATOR = process.env.API_CREATOR || "Welcome to Api Rinn";
 app.use((req, res, next) => {
     const originalJson = res.json;
     res.json = function (data) {
